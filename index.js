f62ba7a516534df477c2798430b2d57b99133503
Jordan Sleeper <survivingknowledge@gmail.com>
2017-04-26 23:54:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function getIssues() {
+
+  return fetch(`api.github.com/javascript-fetch-lab/issues`, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
 }
 
 function showIssues(json) {
+  const template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
+  document.getElementById('issues').innerHTML = template(json);
 }
 
 function createIssue() {
+  return fetch(`api.github.com/javascript-fetch-lab/issues`, {
+    method: 'post',
+    body: 'test body',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
 }
 
 function showResults(json) {
+  const template = Handlebars.compile(document.getElementById('repo-template').innerHTML);
+  document.getElementById('results').innerHTML = template(json)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
