5cc661f3890f6f52d9ff5163a7290c355e234a27
Tyler Bristol <tahbristol@gmail.com>
2017-10-14 00:53:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
 function getIssues() {
+  const token = '';
+  const issues = "https://api.github.com/repos/tahbristol/javascript-fetch-lab/issues";
+  fetch(issues, {
+    method: 'get',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => showIssues(res));
+
 }
 
 function showIssues(json) {
+  var issues = document.getElementById('issues');
+  issues.innerHMTL = json.url;
 }
 
 function createIssue() {
+  var title = document.getElementById('title').value;
+  var body = document.getElementById('body').value;
+  const token = '';
+  const issue = 'https://api.github.com/repos/tahbristol/javascript-fetch-lab/issues'
+   fetch(issue, {
+    method: 'post',
+    body: body,
+    title: title,
+    assignee: "tahbristol",
+    headers: {
+    	Authorization: `token ${token}`
+    }
+
+   }).then(res => console.log(res));
 }
 
+
 function showResults(json) {
+  var result = document.getElementById('results');
+  result.innerHMTL = json.url;
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  const postData = {
+    body: "Here we go, Steelers, Here we go!"
+  };
+  const token = '';
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
+  fetch(repo, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => showResults(res.url));
+
+
 }
 
 function getToken() {
