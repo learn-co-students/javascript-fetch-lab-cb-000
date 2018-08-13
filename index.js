312cfc31ab4c9a063ebe720fb253c3cc5647e995
Vishal Sharma <vsh1819@gmail.com>
2018-08-13 05:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,84 @@
+const userName = ''
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
+
+//Issue and Repo objects and templates
+
+function Issue(attributes){
+  this.title = attributes.title;
+  this.body = attributes.body;
+  this.url = attributes.url;
+}
+
+function Repo(attributes){
+  this.url = attributes.url;
+}
+
+Issue.prototype.template = function(){
+   var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
+   return template;
+};
+
+Repo.prototype.template = function(){
+  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+  return template;
+};
+
+
+
+
 function getIssues() {
+  fetch(`${baseApi}repos/${fork}/issues`).
+   then(resp => {
+     resp.json().then( data => {
+       for (let i = 0; i < data.length; i++){
+         displayIssue(new Issue(data[i]));
+       }
+     } )
+   })
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+const issueBody = document.getElementById('body').value
+const postData = { title: issueTitle, body: issueBody }
+fetch(`${baseApi}repos/${fork}/issues`, {
+  method: 'post',
+  headers: {
+    'Authorization': `token ${getToken()}`
+  },
+  body: JSON.stringify(postData)
+}).then(resp => getIssues())
 }
+
+function displayIssue(issue) {
+  $('#issues').append(issue.template())
+}
+
+
 
 function showResults(json) {
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+ fetch(`${baseApi}repos/${repo}/forks`, {
+   method: 'post',
+   headers: {
+     'Authorization': `token ${getToken()}`
+   }
+ }).then(resp => {
+   let repo = new Repo(resp);
+   showForkedRepo(repo);
+ })
+}
+
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
 }
 
 function getToken() {
